5da6bd17df1b33e03920aa4084027c38fda86ac9
Andrew C <acraig176@hotmail.com>
2024-10-08 15:09:53 +0100

    Fix workflows

--- backend/bin/entrypoint.sh
+++ backend/bin/entrypoint.sh
@@ -5,7 +5,6 @@ cd /app/src
 
 # Generate a swagger json
 go install github.com/swaggo/swag/cmd/swag@v1.8.12
-echo "Generating swagger json"
 swag init
 
 # Run the API
--- init.sql
+++
@@ -1,4 +0,0 @@
--- This SQL file is used to init MariaDB on container startup
-
-DROP DATABASE IF EXISTS com668;
-CREATE DATABASE IF NOT EXISTS com668;
