db4324f1a4577782a9e10bc6358b1fc5530226e4
Andrew C <acraig176@hotmail.com>
2025-01-15 10:41:40 +0000

    Forward query params

--- frontend/src/app/api/[...endpoint]/route.tsx
+++ frontend/src/app/api/[...endpoint]/route.tsx
@@ -1,14 +1,22 @@
 import { NextRequest } from "next/server";
 
+const host = "com668-backend:5000";
+
 export async function GET(request: NextRequest, { params }: { params: { endpoint: string[] } }) {
-    return await fetch(`https://com668-backend:5000/${params.endpoint.join("/")}`, {
+    const { endpoint } = await params;
+    const queryStrings = Array.from(request.nextUrl.searchParams.entries()).map(([key, value]) => `${key}=${value}`);
+    const query = queryStrings.length > 0 ? `?${queryStrings.join("&")}` : "";
+    return await fetch(`https://${host}/${endpoint.join("/")}${query}`, {
         method: "GET",
         headers: request.headers
     });
 }
 
 export async function POST(request: NextRequest, { params }: { params: { endpoint: string[] } }) {
-    return await fetch(`https://com668-backend:5000/${params.endpoint.join("/")}`, {
+    const { endpoint } = await params;
+    const queryStrings = Array.from(request.nextUrl.searchParams.entries()).map(([key, value]) => `${key}=${value}`);
+    const query = queryStrings.length > 0 ? `?${queryStrings.join("&")}` : "";
+    return await fetch(`https://${host}/${endpoint.join("/")}${query}`, {
         method: "POST",
         body: await request.text(),
         headers: request.headers
@@ -16,7 +24,10 @@ export async function POST(request: NextRequest, { params }: { params: { endpoin
 }
 
 export async function PATCH(request: NextRequest, { params }: { params: { endpoint: string[] } }) {
-    return await fetch(`https://com668-backend:5000/${params.endpoint.join("/")}`, {
+    const { endpoint } = await params;
+    const queryStrings = Array.from(request.nextUrl.searchParams.entries()).map(([key, value]) => `${key}=${value}`);
+    const query = queryStrings.length > 0 ? `?${queryStrings.join("&")}` : "";
+    return await fetch(`https://${host}/${endpoint.join("/")}${query}`, {
         method: "PATCH",
         body: await request.text(),
         headers: request.headers
@@ -24,7 +35,10 @@ export async function PATCH(request: NextRequest, { params }: { params: { endpoi
 }
 
 export async function PUT(request: NextRequest, { params }: { params: { endpoint: string[] } }) {
-    return await fetch(`https://com668-backend:5000/${params.endpoint.join("/")}`, {
+    const { endpoint } = await params;
+    const queryStrings = Array.from(request.nextUrl.searchParams.entries()).map(([key, value]) => `${key}=${value}`);
+    const query = queryStrings.length > 0 ? `?${queryStrings.join("&")}` : "";
+    return await fetch(`https://${host}/${endpoint.join("/")}${query}`, {
         method: "PUT",
         body: await request.text(),
         headers: request.headers
@@ -32,7 +46,10 @@ export async function PUT(request: NextRequest, { params }: { params: { endpoint
 }
 
 export async function DELETE(request: NextRequest, { params }: { params: { endpoint: string[] } }) {
-    return await fetch(`https://com668-backend:5000/${params.endpoint.join("/")}`, {
+    const { endpoint } = await params;
+    const queryStrings = Array.from(request.nextUrl.searchParams.entries()).map(([key, value]) => `${key}=${value}`);
+    const query = queryStrings.length > 0 ? `?${queryStrings.join("&")}` : "";
+    return await fetch(`https://${host}/${endpoint.join("/")}${query}`, {
         method: "DELETE",
         headers: request.headers
     });
