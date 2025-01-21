4fd52298756794737b1a5ac97fc88bdb2d61f42a
Andrew C <acraig176@hotmail.com>
2025-01-21 14:22:07 +0000

    Redesign navbar

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -2,7 +2,21 @@
 
 import { redirect, RedirectType, usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
-import { Navbar, NavbarText, NavItem, NavLink } from "react-bootstrap";
+import {
+    Col,
+    Dropdown,
+    DropdownDivider,
+    DropdownItem,
+    DropdownItemText,
+    DropdownMenu,
+    DropdownToggle,
+    Nav,
+    Navbar,
+    NavItem,
+    NavLink,
+    Row
+} from "react-bootstrap";
+import { Slack } from "react-bootstrap-icons";
 
 export default function NavbarComponent() {
     const pathname = usePathname();
@@ -35,34 +49,43 @@ export default function NavbarComponent() {
 
     return (
         <Navbar className="mt-1 mx-2 p-2 border-b">
-            <NavItem className="p-2 m-1 border rounded" hidden={user == null}>
-                <NavLink href="/dashboard">Dashboard</NavLink>
-            </NavItem>
-            <NavItem className="p-2 m-1 border rounded" hidden={user == null || user["admin"] == false}>
-                <NavLink href="/settings">Settings</NavLink>
-            </NavItem>
-            <NavItem className="p-2 m-1 border rounded" hidden={user == null || user["admin"] == false}>
-                <NavLink href="/hosts">Host Inventory</NavLink>
-            </NavItem>
+            <Nav activeKey={pathname}>
+                <NavItem className="p-2 m-1 border rounded" hidden={user == null}>
+                    <NavLink href="/dashboard" eventKey="/dashboard">Dashboard</NavLink>
+                </NavItem>
+                <NavItem className="p-2 m-1 border rounded" hidden={user == null || user["admin"] == false}>
+                    <NavLink href="/settings" eventKey="/settings">Settings</NavLink>
+                </NavItem>
+                <NavItem className="p-2 m-1 border rounded" hidden={user == null || user["admin"] == false}>
+                    <NavLink href="/hosts" eventKey="/hosts">Host Inventory</NavLink>
+                </NavItem>
+            </Nav>
             <NavItem className="me-auto"></NavItem>
-            { user != null
-              ? (<NavItem className="p-2 m-1">
-                    <NavbarText>Signed in as <span style={{color: "grey"}}>{user["name"]}</span></NavbarText>
-                </NavItem>)
-              : (<NavItem className="p-2 m-1">
-                    <NavbarText>Not logged in</NavbarText>
-                </NavItem>)
-            }
             <NavItem hidden={user == null}>
-                <NavLink href="#auth" style={{alignItems: "center", color: "#fff", backgroundColor: "#4A154B", border: 0, borderRadius: "48px", display: "inline-flex", fontFamily: "Lato, sans-serif", fontSize: "16px", fontWeight: 600, height: "48px", justifyContent: "center", textDecoration: "none", width: "190px"}} onClick={authSlack}>
-                    <svg xmlns="http://www.w3.org/2000/svg" style={{height: "20px", width: "20px", marginRight: "12px"}} viewBox="0 0 122.8 122.8">
-                        <path d="M25.8 77.6c0 7.1-5.8 12.9-12.9 12.9S0 84.7 0 77.6s5.8-12.9 12.9-12.9h12.9v12.9zm6.5 0c0-7.1 5.8-12.9 12.9-12.9s12.9 5.8 12.9 12.9v32.3c0 7.1-5.8 12.9-12.9 12.9s-12.9-5.8-12.9-12.9V77.6z" fill="#e01e5a" />
-                        <path d="M45.2 25.8c-7.1 0-12.9-5.8-12.9-12.9S38.1 0 45.2 0s12.9 5.8 12.9 12.9v12.9H45.2zm0 6.5c7.1 0 12.9 5.8 12.9 12.9s-5.8 12.9-12.9 12.9H12.9C5.8 58.1 0 52.3 0 45.2s5.8-12.9 12.9-12.9h32.3z" fill="#36c5f0" />
-                        <path d="M97 45.2c0-7.1 5.8-12.9 12.9-12.9s12.9 5.8 12.9 12.9-5.8 12.9-12.9 12.9H97V45.2zm-6.5 0c0 7.1-5.8 12.9-12.9 12.9s-12.9-5.8-12.9-12.9V12.9C64.7 5.8 70.5 0 77.6 0s12.9 5.8 12.9 12.9v32.3z" fill="#2eb67d" />
-                        <path d="M77.6 97c7.1 0 12.9 5.8 12.9 12.9s-5.8 12.9-12.9 12.9-12.9-5.8-12.9-12.9V97h12.9zm0-6.5c-7.1 0-12.9-5.8-12.9-12.9s5.8-12.9 12.9-12.9h32.3c7.1 0 12.9 5.8 12.9 12.9s-5.8 12.9-12.9 12.9H77.6z" fill="#ecb22e" />
-                    </svg>
-                    Authorise Slack
-                </NavLink>
+                <Dropdown>
+                    <DropdownToggle>User</DropdownToggle>
+                    <DropdownMenu align="end" style={{minWidth: "13rem"}}>
+                        { user == null
+                            ? (<DropdownItemText>Not logged in</DropdownItemText>)
+                            : (<DropdownItemText>Signed in as <span style={{color: "grey"}}>{user["name"]}</span></DropdownItemText>)
+                        }
+                        { user != null && (
+                            <>
+                                <DropdownDivider />
+                                <DropdownItem onClick={authSlack}>
+                                    <Row xs={4}>
+                                        <Col xs={1}>
+                                            <Slack />
+                                        </Col>
+                                        <Col xs={3}>
+                                            Connect with Slack
+                                        </Col>
+                                    </Row>
+                                </DropdownItem>
+                            </>
+                        )}
+                    </DropdownMenu>
+                </Dropdown>
             </NavItem>
         </Navbar>
     );
